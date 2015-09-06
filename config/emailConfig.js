2f004b5066d69d8993e97ca00b600aeeb3617b38
ydasari <ydasari@open-logix.com>
2015-09-06 12:37:14 -0400

    Update emailConfig.js

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -2,8 +2,8 @@ var nodemailer = require("nodemailer");
 var smtpTransport = nodemailer.createTransport("SMTP",{
 	service: "Gmail",
 	auth: {
-		user: "dasariyeshwant@gmail.com",
-		pass: "enigmaYEIS100"
+		user: "",
+		pass: ""
 	}
 });
 module.exports = smtpTransport;
